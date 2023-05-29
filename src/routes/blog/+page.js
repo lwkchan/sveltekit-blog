8185fb2bfc230075e1f4ca2a5a72d166530ef386
Laura Chan <laurachan205@gmail.com>
2023-05-29 10:09:32 +0800

    Reverse order of blogposts

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -25,7 +25,7 @@ export async function load() {
 	blogposts = blogposts.sort((a, b) => {
 		const aDate = new Date(a.date);
 		const bDate = new Date(b.date);
-		return aDate.getTime() - bDate.getTime();
+		return bDate.getTime() - aDate.getTime();
 	});
 
 	return {
